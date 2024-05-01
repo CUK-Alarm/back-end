30971dadd1bfcb06558356fa21750d802c5c2cc5
HyunTae Jo <hyuntae9912@naver.com>
2024-05-01 22:18:29 +0900

    리팩토링 + 주석 수정

--- src/main/java/com/hyunn/alarm/dto/request/UserDepartmentRequest.java
+++ src/main/java/com/hyunn/alarm/dto/request/UserDepartmentRequest.java
@@ -1,29 +1,18 @@
 package com.hyunn.alarm.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserDepartmentRequest {
 
   @NotBlank(message = "핸드폰 번호를 입력해주세요.")
-  @Pattern(regexp = "^\\d{11}$", message = "핸드폰 번호는 11자리여야 합니다.")
   private String phone;
 
-  @NotBlank(message = "전공을 입력해주세요.")
+  @NotBlank(message = "제 1전공을 입력해주세요.")
   private String major;
 
-  @NotBlank(message = "전공을 입력해주세요.")
+  @NotBlank(message = "제 2전공을 입력해주세요. (전공심화라면 같은 전공을 선택해주세요.)")
   private String minor;
 
-  public UserDepartmentRequest(String phone, String major, String minor) {
-    this.phone = phone;
-    this.major = major;
-    this.minor = minor;
-  }
-
-  public static UserDepartmentRequest create(String phone, String major, String minor) {
-    return new UserDepartmentRequest(phone, major, minor);
-  }
 }
